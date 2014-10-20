a566cb224de223a394c1a8d90bdae5e5740b12cb
SveinEven <sveineven.jo@gmail.com>
2014-10-20 14:58:59 +0200

    Meny

---
+++ Assets/Script/LoadGame.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+private var displayLabel = false;
+private var height = Screen.height;
+private var width = Screen.width;
+
+function Start() {
+FlashLabel();
+}
+ 
+function FlashLabel() {
+   
+// Fancy pants flash of label on and off   
+	while (1) {
+		displayLabel = true;
+		yield WaitForSeconds(.5);
+		displayLabel = false;
+		yield WaitForSeconds(.5); 
+	}
+ 
+}
+var customSkin : GUISkin;
+function OnGUI() {
+	GUI.skin = customSkin;
+ 	if (displayLabel == true)
+ 	GUI.Label(Rect (0, 0, width, height),"LOADING");
+ 
+}
--- Assets/Script/TitleGUI.js
+++ Assets/Script/TitleGUI.js
@@ -1,9 +1,56 @@
 ﻿var customSkin : GUISkin;
+private var main : boolean = true;
+private var graphic : boolean = false;
+private var displayLabel = false;
+private var teller : int = 5;
+
+function Start() {
+FlashLabel();
+}
+
+function FlashLabel() {
+// Fancy pants flash of label on and off   
+	while (1) {
+		displayLabel = true;
+		yield WaitForSeconds(.5);
+		displayLabel = false;
+		yield WaitForSeconds(.5); 
+	}
+	}
 
 function OnGUI(){
 	GUI.skin = customSkin;
-	if(GUI.Button(Rect(0, 0, 200, 100), "Click me 1!")){
-		Application.LoadLevel ("StonedGame");
+	if (main == true){
+		if(GUI.Button(Rect(0, 0, 200, 100), "Play Game!")){
+			Application.LoadLevel ("StonedGame");
+			main = false;
+		};
+		if(GUI.Button(Rect(0, 100, 200, 100), "Graphic!")){
+			main = false;
+			graphic = true;
+		};
 	};
-
+	if(graphic == true){
+		GUI.Label(Rect (0, 150, 200, 100), "Graphic level:  " + teller);
+		if (GUI.Button(Rect(0, 0, 200, 100), "Decrease")) {
+			QualitySettings.DecreaseLevel();
+			teller --;
+		};
+		if (GUI.Button(Rect(0, 100, 200, 100), "Increase")) {
+			QualitySettings.IncreaseLevel();
+			teller ++;
+		};
+		if (teller > 6){
+			teller = 6;
+		};
+		if (teller < 1){
+			teller = 1;
+		};
+		
+		if (GUI.Button(Rect(0, Screen.height-75, 150, 75), "Back")) {
+			main = true;
+			graphic = false;
+		};
+	};
+	
 }
