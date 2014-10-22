a5b6f62cb1c40b13bdeb1d2e99192593565a75d5
SveinEven <sveineven.jo@gmail.com>
2014-10-22 16:19:24 +0200

    WE HAVE GEMS!!!!!!!

--- Assets/Script/CharTeller.js
+++ Assets/Script/CharTeller.js
@@ -6,5 +6,5 @@ teller = 0;
 
 function OnGUI(){
 	GUI.skin = customSkin;
-	GUI.Label(Rect (0, 0, Screen.width, Screen.height), teller +" steiner samlet av: 300");
+	GUI.Label(Rect (0, 0, Screen.width, Screen.height), teller +"/300");
 }
---
+++ Assets/Script/Meny.js
@@ -0,0 +1,29 @@
+﻿private var meny : boolean = false;
+var customSkin : GUISkin;
+
+function OnGUI(){
+	GUI.skin = customSkin;
+	if (meny == true){
+		RestartMenu();
+	}
+}
+
+function Update(){
+	if(Input.GetKeyUp(KeyCode.Escape)){
+		Debug.Log("Test1");
+		if (meny == false){
+			meny = true;
+		} else{
+			meny = false;
+		};
+	};
+}
+
+function RestartMenu(){
+GUI.BeginGroup(Rect(Screen.width/3, Screen.height/5, Screen.width/3, Screen.height/1.5));
+GUI.Box (new Rect(0, 0, Screen.width/3, Screen.height/1.5), "Menu");
+	if (GUI.Button(Rect(0, 75, Screen.width/3, 50), "Restart")) {
+		Application.LoadLevel (Application.loadedLevel);
+	};
+GUI.EndGroup();
+}
