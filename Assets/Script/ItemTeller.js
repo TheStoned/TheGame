c195eb7b65705f3ca7bbe0ebf1b86b27ca605bcc
SveinEven <sveineven.jo@gmail.com>
2014-10-22 14:39:37 +0200

    Item system

---
+++ Assets/Script/ItemTeller.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+private var teller : int;
+private var charTeller : CharTeller;
+charTeller = GameObject.Find("Sphere").GetComponent(CharTeller);
+private var object = GameObject;
+
+
+function OnTriggerEnter (collision : Collider){
+	Destroy(gameObject);
+	teller = charTeller.teller;
+	Debug.Log(teller);
+	teller ++;
+	charTeller.teller = teller;
+	
+
+}
